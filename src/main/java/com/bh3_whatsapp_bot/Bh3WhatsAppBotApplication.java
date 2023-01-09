d69f79a9f0d64a1aca040a188a7ba93d97f9b87a
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-01-09 10:57:25 +0200

    Initial commit

---
+++ src/main/java/com/bh3_whatsapp_bot/Bh3WhatsAppBotApplication.java
@@ -0,0 +1,13 @@
+package com.bh3_whatsapp_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bh3WhatsAppBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bh3WhatsAppBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bh3_whatsapp_bot/Bh3WhatsAppBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bh3_whatsapp_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bh3WhatsAppBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
